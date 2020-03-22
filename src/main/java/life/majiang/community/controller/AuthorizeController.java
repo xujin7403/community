6646c82055e90f6414158c5944631f8a5c453796
xujin7403 <m13415348996@163.com>
2020-03-22 22:27:09 +0800

    add userInfo is existence

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -35,7 +37,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state,
-                           HttpServletRequest request){
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -48,14 +51,23 @@ public class AuthorizeController {
 
         if (githubUser != null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccount_id(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
-            userMapper.insert(user);
-            //登录成功，写入cookie和session
-            request.getSession().setAttribute("user", user);
+
+            User userInfo = userMapper.findByAccountId(user.getAccount_id());
+            if(userInfo != null) {
+                System.out.println("用户已存在！");
+                response.addCookie(new Cookie("token", userInfo.getToken()));
+            }else {
+                System.out.println("新用户登录！");
+                userMapper.insert(user);
+                //登录成功，写入cookie和session
+                response.addCookie(new Cookie("token", token));
+            }
             return "redirect:/";//重定向
 
         }else {
