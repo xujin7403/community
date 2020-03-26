21aab128abf8a74f53d02a94ab1e7ec328f293cb
xujin7403 <m13415348996@163.com>
2020-03-26 21:45:35 +0800

    add fabu

---
+++ src/main/java/life/majiang/community/mapper/QuestionMapper.java
@@ -0,0 +1,11 @@
+package life.majiang.community.mapper;
+
+import life.majiang.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into question(title, description, gmt_create, gmt_modified, creator, comment_count, view_count, like_count, tag) value(#{title}, #{description}, #{gmtCreate}, #{gmtModified}, #{creator}, #{commentCount}, #{viewCount}, #{likeCount}, #{tag})")
+    void create(Question question);
+}
